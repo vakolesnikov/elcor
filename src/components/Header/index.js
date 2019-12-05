b9da9f0ae2c24e0a96f4cac5f301ff5b04a6b5b4
Vasiliy Kolesnikov <v.a.kolesnikov@bk.ru>
2019-12-05 22:07:30 +0300

    fix after review

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,26 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import elcorLogo from '../../assets/elcor-logo.png';
 
+import './index.css';
+
 export default class Header extends React.Component {
     render() {
         return (
             <header className="main-header">
-                <div className="container main-header-content">
-                    <div className="top-navigation-box">
-                        <Link to="/" className="main-logo">
-                            <div className="main-logo_title">Elcor</div>
-                            <img className="main-logo_image" src={elcorLogo} alt="elcor-logo" />
-                            <span className="main-logo_address">ТЦ "ЦУМ", 1й этаж</span>
-                        </Link>
+                <div className="main-header-content">
+                    <div className="top-header-box">
+                        <div className="top-header-container">
+                            <Link to="/" className="main-logo">
+                                <div className="main-logo_title">Elcor</div>
+                                <img className="main-logo_image" src={elcorLogo} alt="elcor-logo" />
+                            </Link>
 
-                        <div className="contacts-container">
-                            <a href="tel:+79631033030" className="phone-container">
-                                <span className="material-icons phone-icon">local_phones</span>
-                                <span className="phone-number">8 (963)103-30-30</span>
-                            </a>
+                            <div className="contacts-container">
+                                <a href="tel:+79631033030" className="phone-container">
+                                    <span className="material-icons phone-icon">local_phones</span>
+                                    <span className="phone-number">8 (963)103-30-30</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="top-header-container">
+                            <span className="main-logo_address">ТЦ "ЦУМ", 1й этаж</span>
+
                             <span className="work-time">С 9:00 до 21:00, без выходных</span>
                         </div>
                     </div>
+
                     <nav className="main-navigation">
                         <ul className="navigation-list">
                             <li className="navigation-item">
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import ProductItem from '../ProductItem';
 
 import logo1 from '../../assets/logo-1.png';
@@ -17,8 +18,7 @@ import xiaomiLogo from '../../assets/xiaomiLogo.png';
 import samsungLogo from '../../assets/samsungLogo.png';
 
 import elcorLogo from '../../assets/elcor-logo.png';
-import Map from "../Map";
-import {Link} from "react-router-dom";
+import Map from '../Map';
 
 const products = [
     { title: 'apple', img: apple },
@@ -26,11 +26,17 @@ const products = [
     { title: 'xiaomi', img: xiaomi }
 ];
 
+const advantagesItems = [
+    { title: 'Уровень самоотдачи', value: 146 },
+    { title: 'Соблюдение сроков', value: 100 },
+    { title: 'Минимальная предоплата', value: 30 }
+];
+
 export default class Main extends React.Component {
     render() {
         return (
             <div className="container">
-                <main>
+                <main className="main">
                     <div className="main-banner">
                         <div className="main-banner-item main-banner-item1">
                             <section className="main-banner-information">
@@ -45,27 +51,27 @@ export default class Main extends React.Component {
                             </section>
                         </div>
                     </div>
-                    {/*<div className="product">*/}
-                    {/*    {products.map((product, index) => (*/}
-                    {/*        <ProductItem title={product.title} key={index} img={product.img} />*/}
-                    {/*    ))}*/}
-                    {/*</div>*/}
+                    {/* <div className="product"> */}
+                    {/*    {products.map((product, index) => ( */}
+                    {/*        <ProductItem title={product.title} key={index} img={product.img} /> */}
+                    {/*    ))} */}
+                    {/* </div> */}
                     <div className="nerds-information">
                         <div className="information-box">
                             <h2 className="nerds-information-title">
                                 Мы — маленький, но гордый магазин из Пензы.
                             </h2>
                             <div className="nerds-information-text">
-                                Исповедуем принципы минимализма и чистоты. Ставим счастье и радость
-                                клиента превыше собственной выгоды. Работаем не покладая рук, как
-                                роботы.
+                                Ставим счастье и радость клиента превыше собственной выгоды.
+                                Работаем не покладая рук, как роботы.
                             </div>
                             <div className="nerds-information-product">
                                 <b>Выполняем заказы на разработку :</b>
                                 <ul className="nerds-product-list">
-                                    <li>Наш стиль</li>
-                                    <li>Наш скилл</li>
-                                    <li>Наша фишка</li>
+                                    <li>Рассрочка 1% в месяц</li>
+                                    <li>Гарантия (30 дней на Б/У - 1 год на новый)</li>
+                                    <li>Trade-in (обмен Вашей техники на новую)</li>
+                                    <li>Бесплатная доставка по городу</li>
                                 </ul>
                             </div>
                         </div>
@@ -76,37 +82,19 @@ export default class Main extends React.Component {
                             <div className="advantages-box">
                                 <div className="advantages-title">Любим точность во всем:</div>
                                 <div className="advantages-content">
-                                    <div className="advantages-item">
-                                        <span>
-                                            146<sup>%</sup>
-                                        </span>
-                                        Уровень самоотдачи
-                                    </div>
-                                    <div className="advantages-item">
-                                        <span>
-                                            100<sup>%</sup>
-                                        </span>
-                                        Соблюдение сроков
-                                    </div>
-                                    <div className="advantages-item">
-                                        <span>
-                                            50<sup>%</sup>
-                                        </span>
-                                        Минимальная предоплата
-                                    </div>
+                                    {advantagesItems.map(({ title, value }) => (
+                                        <div className="advantages-item" key={title}>
+                                            <span>
+                                                {value}
+                                                <sup>%</sup>
+                                            </span>
+                                            {title}
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                         </div>
                     </div>
-                    {/*<div className="partners">*/}
-                    {/*    <img src={appleLogo} alt="images" className="partners-item" />*/}
-
-                    {/*    <img src={samsungLogo} alt="images" className="partners-item" />*/}
-
-                    {/*    <img src={xiaomiLogo} alt="images" className="partners-item" />*/}
-
-                    {/*    <img src={logo4} alt="images" className="partners-item" />*/}
-                    {/*</div>*/}
                     <Map />
                 </main>
             </div>
