8048f4ab608892dbf5ae549afa2c50cc9e4f6fca
Vasiliy Kolesnikov <v.a.kolesnikov@bk.ru>
2019-12-15 22:27:09 +0300

    card info complete

--- src/components/ProductItem/index.js
+++ src/components/ProductItem/index.js
@@ -10,41 +10,55 @@ class ProductItem extends React.Component {
         super();
 
         this.state = {
-            isOpenCard: false
+            isOpenCard: false,
+            selectedMainOptionIndex: 0,
+            openServiceTab: 'description'
         };
     }
 
     renderCard = () => {
-        const { title, prices, images } = this.props;
+        const { name, options, prices, images } = this.props;
+        const { selectedMainOptionIndex } = this.state;
 
         return (
             <div className={styles['product-card']}>
                 <div className={styles['product-card-content']}>
                     <div className={styles['main-product-information']}>
                         <div className={styles['product-card-information']}>
-                            <h2 className={styles['card-title']}>{title}</h2>
-                            <img
-                                className={styles['card-image']}
-                                src={samsungImg}
-                                alt="samsungImg"
-                            />
+                            <h2 className={styles['card-title']}>{name}</h2>
+                            <div className={styles['card-image']}>
+                                <img
+                                    src={`http://elcor.herokuapp.com/images/${images[0]}`}
+                                    alt="samsungImg"
+                                />
+                            </div>
                         </div>
                         <div className={styles['product-card-options']}>
                             <div className={styles['card-options-item']}>
                                 <span className={styles['options-title']}>Накопитель:</span>
-                                <span
-                                    className={`${styles['option-param']} ${
-                                        styles['selected-option-param']
-                                    }`}
-                                >
-                                    32
-                                </span>
-                                <span className={styles['option-param']}>128</span>
+                                {options.memory.map((value, index) => (
+                                    <button
+                                        type="button"
+                                        className={`${styles['option-param']} ${
+                                            selectedMainOptionIndex === index
+                                                ? styles['selected-option-param']
+                                                : ''
+                                        }`}
+                                        key={value}
+                                        onClick={() =>
+                                            this.setState({ selectedMainOptionIndex: index })
+                                        }
+                                    >
+                                        {value}
+                                    </button>
+                                ))}
                             </div>
 
                             <div className={styles['card-options-item']}>
                                 <span className={styles['options-title']}>Цена:</span>
-                                <span className={styles['price-value']}>18990 р.</span>
+                                <span className={styles['price-value']}>
+                                    {prices[selectedMainOptionIndex]}
+                                </span>
                             </div>
 
                             <div className={styles['card-options-item']}>
@@ -53,61 +67,128 @@ class ProductItem extends React.Component {
                                 </button>
                             </div>
                         </div>
-                        <button
-                            type="button"
-                            className={styles['card-close-button']}
-                            onClick={() => this.setState({ isOpenCard: false })}
-                        >
-                            +
-                        </button>
                     </div>
-                    <div className={styles['product-description']}>
-                        <div className={styles['description-tabs']}>
+                    <div className={styles['product-service-information']}>
+                        <div className={styles['service-tabs']}>
                             <li
-                                className={`${styles['description-tab']} ${
-                                    styles['description-tab-active']
+                                className={`${styles['service-tab']} ${
+                                    styles['service-tab-active']
                                 }`}
+                                onClick={() => this.setState({ openServiceTab: 'description' })}
                             >
                                 Описание
                             </li>
-                            <li className={styles['description-tab']}>Доставка</li>
-                            <li className={styles['description-tab']}>Оплата</li>
-                            <li className={styles['description-tab']}>Возврат</li>
+                            <li
+                                className={styles['service-tab']}
+                                onClick={() => this.setState({ openServiceTab: 'delivery' })}
+                            >
+                                Доставка
+                            </li>
+                            <li
+                                className={styles['service-tab']}
+                                onClick={() => this.setState({ openServiceTab: 'payment' })}
+                            >
+                                Оплата
+                            </li>
                         </div>
 
-                        <div className={styles['description-content']}>
-                            <h3>Коротко о товаре:</h3>
-                            <div className={styles['description-item']}>- смартфон с iOS 10</div>
-                            <div className={styles['description-item']}>
-                                - экран 4.7", разрешение 1334x750
-                            </div>
-                            <div className={styles['description-item']}>
-                                - камера 12 МП, автофокус
-                            </div>
-                            <div className={styles['description-item']}>
-                                - память 32 Гб, без слота для карт памяти
-                            </div>
-                            <div className={styles['description-item']}>
-                                - 3G, 4G LTE, LTE-A, Wi-Fi, Bluetooth, NFC, GPS, ГЛОНАСС
-                            </div>
-                            <div className={styles['description-item']}>
-                                - объем оперативной памяти 2 Гб
-                            </div>
-                            <div className={styles['description-item']}>
-                                - аккумулятор 1960 мА⋅ч
-                            </div>
-                            <div className={styles['description-item']}>
-                                - вес 138 г, ШxВxТ 67.10x138.30x7.10 мм
-                            </div>
-                        </div>
+                        {this.renderDescriptionContent()}
+                        {this.renderDeliveryContent()}
+                        {this.renderPaymentContent()}
                     </div>
+
+                    <button
+                        type="button"
+                        className={styles['card-close-button']}
+                        onClick={() => this.setState({ isOpenCard: false })}
+                    >
+                        <span className="material-icons">close</span>
+                    </button>
                 </div>
             </div>
         );
     };
 
+    renderDescriptionContent = () => {
+        const { openServiceTab } = this.state;
+
+        return (
+            <div
+                className={`${styles['service-item']} ${styles['description-content']} ${
+                    openServiceTab === 'description' ? styles['service-item-visible'] : ''
+                }`}
+            >
+                <h3>Коротко о товаре:</h3>
+                <div className={styles['description-item']}>- смартфон с iOS 10</div>
+                <div className={styles['description-item']}>- экран 4.7", разрешение 1334x750</div>
+                <div className={styles['description-item']}>- камера 12 МП, автофокус</div>
+                <div className={styles['description-item']}>
+                    - память 32 Гб, без слота для карт памяти
+                </div>
+                <div className={styles['description-item']}>
+                    - 3G, 4G LTE, LTE-A, Wi-Fi, Bluetooth, NFC, GPS, ГЛОНАСС
+                </div>
+                <div className={styles['description-item']}>- объем оперативной памяти 2 Гб</div>
+                <div className={styles['description-item']}>- аккумулятор 1960 мА⋅ч</div>
+                <div className={styles['description-item']}>
+                    - вес 138 г, ШxВxТ 67.10x138.30x7.10 мм
+                </div>
+            </div>
+        );
+    };
+
+    renderDeliveryContent = () => {
+        const { openServiceTab } = this.state;
+
+        return (
+            <div
+                className={`${styles['service-item']} ${styles['delivery-information']} ${
+                    openServiceTab === 'delivery' ? styles['service-item-visible'] : ''
+                }`}
+            >
+                <h3>Доставка заказа может быть осуществлена одним из способов ниже:</h3>
+                <ul>
+                    <li className={styles['delivery-information-item']}>
+                        <b>Доставка курьером</b>
+                        <br />
+                        Доставка производится в удобное для вас время и по удобному для вас адресу.
+                        Срок доставки по Пензе от 1 до 2 дней. Оплата производится при получении
+                        товара наличными, либо переводом денежных средств на карту.
+                    </li>
+                    <li>
+                        <b>Самовывоз из магазина</b>
+                        <br />
+                        Забрать заказ можно в магазине в любой день с 9:00 до 21:00.
+                    </li>
+                </ul>
+            </div>
+        );
+    };
+
+    renderPaymentContent = () => {
+        const { openServiceTab } = this.state;
+
+        return (
+            <div
+                className={`${styles['service-item']} ${styles['delivery-information']} ${
+                    openServiceTab === 'payment' ? styles['service-item-visible'] : ''
+                }`}
+            >
+                <h3>Оплатить заказ можно разными способами:</h3>
+                <ul>
+                    <li className={styles['delivery-information-item']}>
+                        <b>Наличными</b>
+                    </li>
+                    <li>
+                        <b>Банковской картой Visa или MasterCard</b>
+                    </li>
+                </ul>
+            </div>
+        );
+    };
+
     render() {
-        const { title, prices, images } = this.props;
+        const { name, prices, images } = this.props;
         const { isOpenCard } = this.state;
 
         return (
@@ -117,7 +198,7 @@ class ProductItem extends React.Component {
                     src={`http://elcor.herokuapp.com/images/${images[0]}`}
                     alt="apple"
                 />
-                <div className={styles['product-item-title']}>{title}</div>
+                <div className={styles['product-item-title']}>{name}</div>
                 <div className={styles['product-item-panel']}>
                     <div className={styles['product-item-price']}>{prices[0]} р.</div>
                     <button
@@ -135,9 +216,10 @@ class ProductItem extends React.Component {
 }
 
 ProductItem.propTypes = {
-    title: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     prices: PropTypes.arrayOf(PropTypes.number).isRequired,
-    images: PropTypes.arrayOf(PropTypes.string).isRequired
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    options: PropTypes.objectOf(PropTypes.any).isRequired
 };
 
 export default ProductItem;
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -4,27 +4,25 @@ import styles from './index.css';
 import products from './products';
 import ProductItem from '../ProductItem';
 
+const pageTitles = {
+    apple: 'Apple',
+    samsung: 'Samsung',
+    xiaomi: 'Xiaomi',
+    accessory: 'Аксессуары'
+};
+
 const Products = props => {
     const { id: currentProduct } = props.match.params;
+
     return (
         <div className={styles['product-container']}>
-            <h2 className={styles['product-title']}>Apple</h2>
+            <h2 className={styles['product-title']}>{pageTitles[currentProduct]}</h2>
             <div className={styles['product-custom-settings']}>Сортировка: по умолчанию</div>
             <div className={styles['product-list']}>
                 {products
                     .filter(item => item.type === currentProduct)
                     .map((product, index) => {
-                        const { memory, prices, name, images } = product;
-
-                        return (
-                            <ProductItem
-                                title={name}
-                                memory={memory}
-                                prices={prices}
-                                key={`${name}${index}`}
-                                images={images}
-                            />
-                        );
+                        return <ProductItem {...product} key={`${product.name}${index}`} />;
                     })}
             </div>
         </div>
